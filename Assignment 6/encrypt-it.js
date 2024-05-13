18ab691ad6919b48a4581415dc5d91c4efb0d88e
Nick Vang <nick.vang279@gmail.com>
2024-05-13 03:11:50 -0700

    Assignment 6

---
+++ Assignment 6/encrypt-it.js
@@ -0,0 +1,65 @@
+/*
+ * Starter file 
+ */
+(function() {
+  "use strict";
+
+  /**
+   * The starting point in our program, setting up a listener
+   * for the "load" event on the window, signalling the HTML DOM has been constructed
+   * on the page. When this event occurs, the attached function (init) will be called.
+   */
+  window.addEventListener("load", init);
+
+  /**
+   * TODO: Write a function comment using JSDoc.
+   */
+  function init() {
+    // Note: In this function, we usually want to set up our event handlers
+    // for UI elements on the page.
+
+    const encryptBtn = document.querySelector("#encrypt-it");
+    const resetBtn = document.querySelector("#reset");
+
+    encryptBtn.addEventListener("click", handleEncrypt);
+    resetBtn.addEventListener("click", handleReset);
+
+  }
+
+  // Add any other functions in this area (you should not implement your
+  // entire program in the init function, for similar reasons that
+  // you shouldn't write an entire Java program in the main method).
+
+  function handleEncrypt()
+  {
+    let text = document.querySelector("#input-text").value;
+    let encryptedText = shiftCipher(text);
+
+    document.querySelector("#result").textContent = encryptedText;
+
+  }
+
+  function handleReset()
+  {
+    document.querySelector("#input-text").value = "";
+    document.querySelector("#result").textContent = "";
+  }
+
+  function shiftCipher(text) {
+    text = text.toLowerCase();
+    let result = "";
+    for (let i = 0; i < text.length; i++) {
+      if (text[i] < 'a' || text[i] > 'z') {
+        result += text[i];
+      } else if (text[i] == 'z') {
+        result += 'a';
+      } else { // letter is between 'a' and 'y'
+        let letter = text.charCodeAt(i);
+        let resultLetter = String.fromCharCode(letter + 1);
+        result += resultLetter;
+      }
+    }
+    return result;
+  }
+
+})();
